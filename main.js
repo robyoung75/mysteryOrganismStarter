800ae00d29d96b2da6f6c609d844cae3184848e6
rob <52644668+robyoung75@users.noreply.github.com>
2020-06-07 11:54:57 -0600

    bonus .complementStrand() method, notes, cleanup

--- main.js
+++ main.js
@@ -1,26 +1,9 @@
 // DNA Mutation Object:
 
-// Returns a random DNA base
-const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
-  return dnaBases[Math.floor(Math.random() * 4)];
-};
-
-// console.log(returnRandBase()); // logs a random dnaBase
-
-// Returns a random single stand of DNA containing 15 bases
-const mockUpStrand = () => {
-  const newStrand = [];
-  for (let i = 0; i < 15; i++) {
-    newStrand.push(returnRandBase());
-  }
-  return newStrand;
-};
-
 // factory function pAequorFactory, returns an object with properties specimenNum and dna.
 
 /*
-.mutate() is responsible for randomly selecting a base in the object’s dna property and changing the current base to a different base. 
+.mutate() method: is responsible for randomly selecting a base in the object’s dna property and changing the current base to a different base. 
 Then .mutate() will return the object’s dna. For example, if the randomly selected base is the 1st base and it is 'A', the base 
 must be changed to 'T', 'C', or 'G'. But it cannot be 'A' again.
 
@@ -40,6 +23,29 @@ willLikelySurvive() method: returns true if the object’s .dna array contains a
 Otherwise, .willLikelySurvive() returns false.
 */
 
+/*
+.complementStrand() method to the factory function’s object that returns the complementary DNA strand. 
+The rules are that 'A's match with 'T's and vice versa. Also, 'C's match with 'G's and vice versa. 
+Use the .compareDNA() to find the two most related instances of pAequor.
+*/
+
+
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ['A', 'T', 'C', 'G'];
+  return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// console.log(returnRandBase()); // logs a random dnaBase
+
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
+  }
+  return newStrand;
+};
 
 const pAequorFactory = (specimenNum, dna) => {
   return {
@@ -56,7 +62,7 @@ const pAequorFactory = (specimenNum, dna) => {
       } else {
         this.dna[i] = "C";
       }
-      return this.dna;
+      return this.dna;     
     },
     compareDNA(pAequor) {
       let counter = 0;
@@ -89,12 +95,31 @@ const pAequorFactory = (specimenNum, dna) => {
       }
       //console.log(gCounter);
       //console.log(cCounter);
-    } //end of object properties    
+    },
+    complementStrand(pAequor) {
+      let newComplementDNA = [];
+      for (let i = 0; i < 15; i++) {
+        if (pAequor.dna[i] === "T") {
+          this.dna[i] = "A";
+          newComplementDNA.push(this.dna[i]);
+        } else if (pAequor.dna[i] === "A") {
+          this.dna[i] = "T";
+          newComplementDNA.push(this.dna[i]);
+        } else if (pAequor.dna[i] === "G") {
+          this.dna[i] = "C";
+          newComplementDNA.push(this.dna[i]);
+        } else {
+          this.dna[i] = "G";
+          newComplementDNA.push(this.dna[i]);
+        }              
+      }
+     //console.log(newComplementDNA); 
+     return newComplementDNA;      
+    } //end of object properties   
   } 
-} 
+}
 
 // creates 30 instances of pAequor
-
 let sample = [];
 let i = 0;
 while (sample.length < 30) {
@@ -102,18 +127,19 @@ while (sample.length < 30) {
   if (benchMark.willLikelySurvive() === true) {
     i++;
     sample.push(benchMark);    
-    //console.log(sample);
-    //console.log(i);
   }
 }
 
+let pAequor1 = pAequorFactory(1, mockUpStrand()); // creates new object test1.
+let pAequor2 = pAequorFactory(2, mockUpStrand()); // creates new object test2.
 
-let test1 = pAequorFactory(1, mockUpStrand()); // creates new object test1.
-let test2 = pAequorFactory(2, mockUpStrand()); // creates new object test2.
-console.log(sample); // logs sample, the DNA chains with a likelyhood of survival.
-//console.log(test1); // logs test1 object.
-//console.log(test1.mutate()); // Logs object test1 method, mutate();
-//test1.compareDNA(test1) // call on object test1 with an arguement of test1 returns 100%. test2 as an argument will log another value.
+//console.log(pAequor1); // logs pAequor1 object.
+console.log(pAequor1.dna); // logs object pAequor1 dna.
+console.log(pAequor1.mutate()); // Logs object pAequor1 method, mutate();
+//pAequor1.compareDNA(pAequor1) // call on object pAequor1 with an arguement of pAequor1 returns 100%. test2 as an argument will log another value.
+//console.log(sample); // logs sample, the DNA chains with a likelyhood of survival.
+//pAequor1.complementStrand(pAequor1); // call on object pAequor1 with an argument of pAequor1. Logs a complemtary DNA strand.
+
 
 
 
